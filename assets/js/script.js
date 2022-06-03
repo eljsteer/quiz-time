8984244ff94f7c9e91f1e5a841498a12d42d8509
Jason <el.jsteer@gmail.com>
2022-06-03 18:39:30 +1000

    created project folder and files. drafted index.html file with content to use in script file. drafted comments planning the Javascript code.

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// Objects containing:
+    // An index value, questions, options and correct answer
+
+    // function to record timer
+
+// eventlistener on starting quiz
+    //function starts time
+    // Adds question objects randomly to an array
+
+// loop through array to pull questions, options and right answer.
+    //function displays answer options in order randomly
+
+// eventlistener function recording userchoice
+    // function to check whether choice is correct/matches
+    // if conditional to display text if answered correctly or incorrectly
+
+// function to reduce time on timer
+    // if conditional to check if answer is incorrect and reduce time
+
+// function to stop time once last question answered
+    //
+
+// function to record time once last question ansered and quiz finished
+
+
+
